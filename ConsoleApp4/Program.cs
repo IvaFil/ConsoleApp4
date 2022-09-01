477548d944952edf6ced92c39703408ac0bec7a7
IvaFil <filipovichiv@yandex.ru>
2022-09-01 11:15:27 +0300

    add musor

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git!!!!!!!!!!");
+            Console.WriteLine("dwefrwgethry5");
         }
     }
 }
